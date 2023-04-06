736883b1f23b07cb721a09f5c88fbaff99003fc2
Eric KOEUT <erickoeut@gmail.com>
2023-04-06 23:19:01 +0200

    backgound team details

--- frontend/src/pages/team-page/pages/team-details/team-details-page.jsx
+++ frontend/src/pages/team-page/pages/team-details/team-details-page.jsx
@@ -10,20 +10,28 @@ export default function TeamDetailsPage() {
     return (
         <>
             <h1>Team details</h1>
-            <div className={style.playerIndex}>
-                {teamPlayers.map(player => (
-                    <div key={player.id} className={style.playerDetails}>
-                        <h2>
-                            {player.first_name} {player.last_name.toUpperCase()}
-                        </h2>
-                        <p>
-                            {player.position}
-                        </p>
-                        <p>
-                            {player.age} ans
-                        </p>
-                    </div>
-                ))}
+            <div className={style.teamDetails} style={{
+                backgroundImage:`url(${team.logo})`
+                }}>
+
+                <div>
+                    <h2>{team.name}</h2>
+                </div>
+                <div className={style.playerIndex}>
+                    {teamPlayers.map(player => (
+                        <div key={player.id} className={style.playerDetails}>
+                            <h2>
+                                {player.first_name} {player.last_name.toUpperCase()}
+                            </h2>
+                            <p>
+                                {player.position}
+                            </p>
+                            <p>
+                                {player.age} ans
+                            </p>
+                        </div>
+                    ))}
+                </div>
             </div>
         </>)
 }
