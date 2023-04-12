6cf09cc661e35850d2fd8077caf84848ec11d011
Eric <erickoeut@gmail.com>
2023-04-12 22:49:57 +0200

    store

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import route from "./routes/app.route"
 
 import { Provider } from "react-redux"
-import store from '../store/store'
+import store from './store/store'
 
 const router = createBrowserRouter(route)
 
