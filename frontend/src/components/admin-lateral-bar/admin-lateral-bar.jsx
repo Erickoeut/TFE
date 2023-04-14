fa0109fa09755a8b5e68f0a06efee6651cc0090f
Eric <erickoeut@gmail.com>
2023-04-14 23:26:03 +0200

    adminlateralbar

--- frontend/src/components/admin-lateral-bar/admin-lateral-bar.jsx
+++ frontend/src/components/admin-lateral-bar/admin-lateral-bar.jsx
@@ -1,10 +1,23 @@
 import { Link } from 'react-router-dom'
 import style from './admin-lateral-bar.module.scss'
+import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
+import axios from "axios"
 export default function AdminLateralBar(){
+    const user=useSelector(state=>state.user.user)
+    const [team,setTeam]=useState(null)
+    useEffect(()=>{
+        console.log(user.teamId);
+        axios.get(`http://localhost:3000/api/teams/${user.teamId}`)
+            .then(({data})=>{
+                console.log(data);
+                setTeam(data)
+            })
+    },[])
     return(
         <div className={style.AdminLateralBar}>
             <h1>Admin lateral bar</h1>
-
+            {team&&<h2>{team.teamName}</h2>}
             <Link to={"/game-sheet"} >Feuilles de match</Link>
             
         </div>
--- frontend/src/store/reducers/user.reducer.js
+++ frontend/src/store/reducers/user.reducer.js
@@ -1,11 +1,13 @@
 import { createReducer } from "@reduxjs/toolkit"
 import { userConnect } from "../actions/user.action"
 
-const initialState = {
-    user:{accessToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjgxNDgxMjMyfQ.vT47Nj7P6aFQdjjbMHSYbyo_gGpEpRHli6DXP5dJiCw",
-        teamId: 1,
-        username: "user1"
-    }
+const initialState = 
+{
+    user:null
+    // {accessToken:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIxIiwiaWF0IjoxNjgxNDgxMjMyfQ.vT47Nj7P6aFQdjjbMHSYbyo_gGpEpRHli6DXP5dJiCw",
+    //     teamId: 1,
+    //     username: "user1"
+    // }
 }
 
 const userReducer = createReducer(initialState,builder=>{
