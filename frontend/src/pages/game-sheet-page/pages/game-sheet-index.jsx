6b0198abb91d317dd143b15281eb3b650b3be1a1
Eric KOEUT <erickoeut@gmail.com>
2023-04-14 17:30:02 +0200

    gameofteam

--- frontend/src/pages/game-sheet-page/pages/game-sheet-index.jsx
+++ frontend/src/pages/game-sheet-page/pages/game-sheet-index.jsx
@@ -1,3 +1,29 @@
-export default function GameSheetIndex(){
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+export default function GameSheetIndex() {
+    const user = useSelector(state => state.user.user)
+    const [games, setGames] = useState([])
+    useEffect(() => {
+        axios.get(`http://localhost:3000/api/games/team/${user.teamId}`)
+            .then(({ data }) => {
+                setGames(data)
+            })
+    }, [])
     
-}
+    return (
+        <div>
+            <h1>test</h1>
+            <h2>Matchs a domicile</h2>
+            <ul>
+                {games.homeGames && games.homeGames.map(game => <li key={game.id}>Tour{game.round} vs {game.awayTeam.teamName} {!game.finish&&<button> <Link>Inscrire Equipe</Link> </button>}</li>)}
+            </ul>
+            <h2>Matchs à l'extérieur</h2>
+            <ul>
+                {games.awayGames && games.awayGames.map(game => <li key={game.id}>Tour{game.round} vs {game.homeTeam.teamName}</li>)}
+            </ul>
+        </div>
+        
+    )
+}  
