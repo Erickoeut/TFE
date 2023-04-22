ac6b7b74cd190daf6e74dd9b51cf06138c3fc36b
Eric <erickoeut@gmail.com>
2023-04-22 09:51:57 +0200

    about

---
+++ frontend/src/pages/about-page/pages/about-me-page.jsx
@@ -0,0 +1,10 @@
+export default function About1Page(){
+    return(
+        <div>
+            <h1>
+                Moi
+            </h1>
+            <button>←</button>
+        </div>
+    )
+}
